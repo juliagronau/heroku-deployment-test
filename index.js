f5e4ac4b2fb3a81dd14e9b9e4c2f19535a35c30a
julia <77528734+juliagronau@users.noreply.github.com>
2021-09-08 09:13:20 +0200

    Test automatic deploy

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Simple App to test deployment on heroku'));
+app.get('/', (req, res) => res.send('Simple App to test deployment on heroku using the GitHub integration.'));
 
 app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
